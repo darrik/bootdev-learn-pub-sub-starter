8b60fdc755e57ad1cf6b15a7511764a379cbaee8
Rikard Glans <rikard@ecx.se>
2025-03-09 19:10:19 +0100

    ch3-l5

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/darrik/bootdev-learn-pub-sub-starter/internal/gamelogic"
+	"github.com/darrik/bootdev-learn-pub-sub-starter/internal/pubsub"
+	"github.com/darrik/bootdev-learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const connStr = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	dial, err := amqp.Dial(connStr)
+	if err != nil {
+		fmt.Printf("error connecting to rabbitmq: %s\n", err)
+		return
+	}
+	defer dial.Close()
+	fmt.Println("Connection to RabbitMQ established!")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+		return
+	}
+
+	_, _, err = pubsub.DeclareAndBind(dial, routing.ExchangePerilDirect, routing.PauseKey+"."+username, routing.PauseKey, pubsub.TransientQueueType)
+	if err != nil {
+		fmt.Printf("rabbitmq error: %s\n", err)
+		return
+	}
+
+	fmt.Println("Press CTRL-C to quit.")
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
---
+++ internal/pubsub/stuff.go
@@ -0,0 +1,46 @@
+package pubsub
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const (
+	DurableQueueType int = iota
+	TransientQueueType
+)
+
+// simpleQueueType is an enum to represent "durable" or "transient"
+func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, simpleQueueType int) (*amqp.Channel, amqp.Queue, error) {
+	mqchan, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("error creating rabbitmq channel: %s\n", err)
+		return nil, amqp.Queue{}, err
+	}
+
+	durable := false
+	autoDelete := false
+	exclusive := false
+	noWait := false
+
+	switch simpleQueueType {
+	case DurableQueueType:
+		durable = true
+	case TransientQueueType:
+		autoDelete = true
+		exclusive = true
+	}
+
+	queue, err := mqchan.QueueDeclare(queueName, durable, autoDelete, exclusive, noWait, nil)
+	if err != nil {
+		return nil, queue, err
+	}
+
+	err = mqchan.QueueBind(queueName, key, exchange, noWait, nil)
+	if err != nil {
+		return nil, queue, err
+	}
+
+	return mqchan, queue, nil
+}
